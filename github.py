7bcf35c36a6303c305d5363d3725a3b14b42bae9
Anjali2192 <967anjalimishra@gmail.com>
2025-09-26 19:37:23 +0530

    Updated Script

--- github.py
+++ github.py
@@ -3,21 +3,20 @@ import requests
 url = "https://api.github.com/search/repositories"
 
 params = {
-    "q": "python",
-    "sort": "stars",
-    "order": "desc"
-    }
+    "q": "python"
+}
 
 response = requests.get(url, params= params)
 
 if response.status_code == 200:
     data = response.json()
-    for repo in data["items"]:
+    for i, repo in enumerate(data["items"]):
         name = repo["name"]
         repo = repo["watchers"]
-        
-        print(f"repo: {name}")
-        print(f"watchers: {repo}")
+
+        print(f"{i}. repo: {name}")
+        print(f"    watchers: {repo}")
+        print()
 
 else:
     print(f"Error: {response.status_code} - {response.text}")
