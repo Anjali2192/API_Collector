449f32428264ebbb37be48be1bd1aa6c2c5c7d76
Anjali2192 <967anjalimishra@gmail.com>
2025-09-28 23:01:16 +0530

    Saved the output in a json file

--- pexels.py
+++ pexels.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import os
 from dotenv import load_dotenv
 
@@ -6,27 +7,37 @@ load_dotenv()
 
 API_KEY = os.getenv("pexels_KEY")
 
-url = "https://api.pexels.com/v1/search"
+url = "https://api.pexels.com/v1/search?page=1&per_page=80&query=nature"
 headers = {"Authorization": API_KEY}
-params = {"query": "nature"}
 
-response = requests.get(url, headers=headers, params=params)
+photo_list =[]
 
-if response.status_code == 200:
-    data = response.json()
-    #print(json.dumps(data, indent=4))
-    for photo in data["photos"]:
-        photo_id = photo["id"]
-        photo_url = photo["url"]
-        print(f"ID: {photo_id}")
-        print(f"URL: {photo_url}")
-        print()
+while url and len(photo_list) < 1000:
+    response = requests.get(url, headers=headers)    
 
-    limit = response.headers.get("X-Ratelimit-Limit")
-    remaining = response.headers.get("X-Ratelimit-Remaining")
+    if response.status_code == 200:
+        data = response.json()
+        url = data["next_page"]
+        #print(json.dumps(data, indent=4))
+        for photo in data["photos"]:
+            photo_list.append(photo["id"])
+            photo_list.append(photo["url"])
 
-    print(f"rate Limit: {limit}")
-    print(f"Remaining: {remaining}")
-    
-else: 
-    print(f"Error: {response.status_code} - {response.text}")
+            if len(photo_list) >= 1000:
+                break
+            
+        limit = response.headers.get("X-Ratelimit-Limit")
+        remaining = response.headers.get("X-Ratelimit-Remaining")    
+
+        print(f"rate Limit: {limit}")
+        print(f"Remaining: {remaining}")
+        
+    else: 
+        print(f"Error: {response.status_code} - {response.text}")
+        break
+
+# Save data to JSON file
+with open("photo.json", "w") as f:
+    json.dump(photo_list, f, indent=4)
+
+print(F"Saved {len(photo_list)} photos in photo.json")
