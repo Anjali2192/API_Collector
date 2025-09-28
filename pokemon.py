16841750c1b9613944e4dff0a30f942c9741bde9
Anjali2192 <967anjalimishra@gmail.com>
2025-09-28 20:12:32 +0530

    Corrected offset error

--- pokemon.py
+++ pokemon.py
@@ -1,22 +1,20 @@
 import requests
 import json
 
-params = {
-    "limit":20,
-    "offset":0
-}
+offset = 0
 
-base_url = "https://pokeapi.co/api/v2/pokemon"
+base_url = f"https://pokeapi.co/api/v2/pokemon?limit=20&offset={offset}"
 
 pokemon_list = []
 
 while base_url and len(pokemon_list) < 200:
-    response = requests.get(base_url, params=params)
+    response = requests.get(base_url)
 
     if response.status_code == 200:
         data = response.json()
         pokemon_list.extend(data["results"])
         base_url = data["next"]
+        offset += 20
 
     else:
         print(f"Error: {response.status_code} - {response.text}")
