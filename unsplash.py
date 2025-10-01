786239599dd3c33d5287b8394ee885869dfbd2d7
Anjali2192 <967anjalimishra@gmail.com>
2025-10-01 20:20:34 +0530

    Added error handling to the script

--- unsplash.py
+++ unsplash.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 API_KEY = os.getenv("unsplash_KEY")
@@ -18,25 +19,35 @@ page = 1
 all_urls = []
 
 while len(all_urls) < 1000 :
-    params = {
-        "page": page,
-        "per_page": 30,
-        "query": "nature"
-    }
+    try:
+        params = {
+            "page": page,
+            "per_page": 30,
+            "query": "nature"
+        }
 
-    response = requests.get(url, headers=headers, params=params)
-     
-    if response.status_code == 200:
-        data = response.json()
-        page += 1
-        result = data["results"]
-        for photo in result:
-            link = photo["urls"]["regular"]       
-            all_urls.append(link)          
-        print(f"Fetching page: {page}, total collected: {len(all_urls)}")
+        response = requests.get(url, headers=headers, params=params)
+         
+        if response.status_code == 200:
+            data = response.json()
+            page += 1
+            result = data["results"]
+            for photo in result:
+                link = photo["urls"]["regular"]       
+                all_urls.append(link)          
+            print(f"Fetching page: {page}, total collected: {len(all_urls)}")
 
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+
+    except requests.exceptions.Timeout:
+        print("Request timed out. Retrying in 5 sec...")
+        time.sleep(5)
+        continue
+
+    except requests.exceptions.RequestException as e:
+        print("Request failed: ", str(e))
+        break
 
 # Save data to JSON file
 with open("photo.json", "w") as f:
